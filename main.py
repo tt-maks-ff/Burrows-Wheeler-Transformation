854a6ed1c6881c872be92791965aee13773620be
Maksim Kozlovsky <87696860+tt-maks-ff@users.noreply.github.com>
2023-08-16 14:17:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def encode(s):
     for i in range(1, len(s)):
         board.append(board[i-1][-1] + board[i-1][:len(s) - 1])
     board.sort()
-    c, result = 0, ''
+    c, result = 0, ""
     for i in range(len(s)):
         result += board[i][-1]
         if board[i] == s: c = i
@@ -19,3 +19,22 @@ def decode(s, n):
             board[i] = s[i] + board[i]
         board.sort()
     return board[n]
+
+if __name__ == "__main__":
+    a = None
+    while a != 3:
+        print("""What do you want to do?
+        1. Encode
+        2. Decode
+        3. Exit
+        Enter a number you need""")
+        a = int(input())
+        if a == 1:
+            print("Enter a string you want to encode:")
+            print(encode(input()))
+        elif a == 2:
+            print("Enter a encoded string:")
+            s = input()
+            print("Enter a number of row:")
+            r = int(input())
+            print(decode(s, r))
